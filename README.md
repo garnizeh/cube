6ddb817898df80601ac10cbedf50d27cb7077781
garnizeh <garnizeh.dev@gmail.com>
2024-04-15 08:58:31 -0300

    finished chapter 3

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # What is this?
 This is the implementation of the book Build an Orchestrator in Go (Tim Boring)
+
+## Docker client version
+If the client complains about the docker API version, just run the command and set the API to the version needed:
+```
+export DOCKER_API_VERSION=1.44
+```
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 
@@ -12,6 +13,43 @@ import (
 	"github.com/garnizeh/cube/task"
 	"github.com/garnizeh/cube/worker"
 )
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=cube",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
+	result := d.Stop(id)
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
+	}
+
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
+}
 
 func main() {
 	t := task.Task{
@@ -34,7 +72,6 @@ func main() {
 	fmt.Printf("task event: %v\n", te)
 
 	w := worker.Worker{
-		Name:  "worker-1",
 		Queue: *queue.New(),
 		Db:    make(map[uuid.UUID]*task.Task),
 	}
@@ -66,4 +103,12 @@ func main() {
 	}
 
 	fmt.Printf("node: %v\n", n)
+
+	fmt.Printf("create a test container\n")
+	dockerTask, createResult := createContainer()
+
+	time.Sleep(time.Second * 5)
+
+	fmt.Printf("stopping container %s\n", createResult.ContainerId)
+	_ = stopContainer(dockerTask, createResult.ContainerId)
 }
